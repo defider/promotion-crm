47950ea0d80098f41bd1cf5c4ba768efe6e027a8
defider <alxndr444@gmail.com>
2025-02-28 01:19:14 +0300

    Create Resource for Reaction Model

--- app/Http/Controllers/ReactionController.php
+++ app/Http/Controllers/ReactionController.php
@@ -4,44 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreReactionRequest;
 use App\Http\Requests\UpdateReactionRequest;
+use App\Http\Resources\ReactionResource;
 use App\Models\Reaction;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ReactionController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Collection
+    public function index(): AnonymousResourceCollection
     {
-        return Reaction::all();
+        return ReactionResource::collection(Reaction::paginate());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreReactionRequest $request)
+    public function store(StoreReactionRequest $request): ReactionResource
     {
-        return Reaction::create($request->all());
+        return new ReactionResource(Reaction::create($request->all()));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Reaction $reaction): Reaction
+    public function show(Reaction $reaction): ReactionResource
     {
-        return $reaction;
+        return new ReactionResource($reaction);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateReactionRequest $request, Reaction $reaction): Reaction
+    public function update(UpdateReactionRequest $request, Reaction $reaction): ReactionResource
     {
         $reaction->update($request->all());
 
-        return $reaction;
+        return new ReactionResource($reaction);
     }
 
     /**
